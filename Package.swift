65305eb052c542f11b195659032105821c10ef5c
Jed Fox <git@jedfox.com>
2023-03-22 10:05:33 -0400

    Add a Package.swift for SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "llama",
+    products: [
+        .library(name: "llama", targets: ["llama"]),
+    ],
+    targets: [
+        .target(
+            name: "llama",
+            path: ".",
+            exclude: ["main.cpp", "tests"],
+            cSettings: [.unsafeFlags(["-Wno-shorten-64-to-32"])]
+        ),
+    ],
+    cxxLanguageStandard: .cxx11
+)
